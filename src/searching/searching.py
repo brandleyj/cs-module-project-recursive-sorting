b60dcaf53ab22aedaec1274f166d6df7db845ed3
Justin Brandley <brandleyj@gmail.com>
2020-06-16 17:53:48 -0500

    finshed searching

--- src/searching/searching.py
+++ src/searching/searching.py
@@ -1,6 +1,17 @@
 # TO-DO: Implement a recursive implementation of binary search
 def binary_search(arr, target, start, end):
     # Your code here
+    found = -1
+
+    if start <= end:
+        center = (start + end) // 2
+        if arr[center] == target:
+            found = center
+        elif arr[center] < target:
+            return binary_search(arr, target, center + 1, end)
+        else:
+            return binary_search(arr, target, start, center - 1)
+    return found
 
 
 # STRETCH: implement an order-agnostic binary search
@@ -9,6 +20,5 @@ def binary_search(arr, target, start, end):
 # sorted in ascending order or in descending order
 # You can implement this function either recursively 
 # or iteratively
-def agnostic_binary_search(arr, target):
-    # Your code here
-
+# def agnostic_binary_search(arr, target):
+#     # Your code here
